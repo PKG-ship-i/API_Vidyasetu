297c113a4e33b287200d3e09545ab7d8afee31db
Pankaj Garg <Pankajg@itpathsolutions.com>
2025-07-05 00:02:22 +0530

    added controller

--- Vidyasetu_API/Controllers/QuizController.cs
+++ Vidyasetu_API/Controllers/QuizController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Vidyasetu_API.DTOs.Request;
+using Vidyasetu_API.Services;
 
 namespace Vidyasetu_API.Controllers
 {
@@ -7,5 +9,56 @@ namespace Vidyasetu_API.Controllers
 	[ApiController]
 	public class QuizController : ControllerBase
 	{
+		private readonly IQuizService _quizService;
+
+		public QuizController(IQuizService quizService)
+		{
+			_quizService = quizService;
+		}
+
+		// POST: /api/quiz/share
+		[HttpPost("share")]
+		public async Task<IActionResult> ShareQuiz([FromBody] ShareQuizRequest request)
+		{
+			// For now, simulate current user (in hackathon, assume userId = 1)
+			long currentUserId = 1;
+
+			string shareCode = await _quizService.ShareQuizAsync(request, currentUserId);
+			return Ok(new
+			{
+				shareCode,
+				shareUrl = $"https://dronaops.com/quiz/{shareCode}"
+			});
+		}
+
+		[HttpPost("{shareCode}/join")]
+		public async Task<IActionResult> JoinQuiz(string shareCode, [FromBody] JoinQuizRequest request)
+		{
+			var participantId = await _quizService.CreateParticipantAsync(shareCode, request);
+			var quiz = await _quizService.GetQuizByShareCodeAsync(shareCode);
+
+			return Ok(new
+			{
+				participantId,
+				quiz
+			});
+		}
+
+
+		// POST: /api/quiz/{shareCode}/submit
+		[HttpPost("{shareCode}/submit")]
+		public async Task<IActionResult> SubmitAnswers(string shareCode, [FromBody] SubmitQuizAnswersRequest request)
+		{
+			await _quizService.SubmitAnswersAsync(shareCode, request);
+			return Ok(new { message = "Answers submitted successfully!" });
+		}
+
+		// GET: /api/quiz/{shareCode}/leaderboard
+		[HttpGet("{shareCode}/leaderboard")]
+		public async Task<IActionResult> GetLeaderboard(string shareCode)
+		{
+			var leaderboard = await _quizService.GetLeaderboardAsync(shareCode);
+			return Ok(leaderboard);
+		}
 	}
 }
